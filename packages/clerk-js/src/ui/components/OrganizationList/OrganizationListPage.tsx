96c3eca8e73f5bde6f4b674f60345a54e6d8c731
panteliselef <panteliselef@outlook.com>
2023-08-31 11:38:56 +0300

    feat(clerk-js): Integrate the createOrganization functionality within OrganizationList

--- packages/clerk-js/src/ui/components/OrganizationList/OrganizationListPage.tsx
+++ packages/clerk-js/src/ui/components/OrganizationList/OrganizationListPage.tsx
@@ -1,7 +1,10 @@
-import { useCoreClerk, useCoreOrganizationList, useEnvironment, useOrganizationListContext } from '../../contexts';
-import { Button, Col, descriptors, Flex, localizationKeys, Spinner } from '../../customizables';
+import { useState } from 'react';
+
+import { useCoreOrganizationList, useEnvironment, useOrganizationListContext } from '../../contexts';
+import { Box, Button, Col, descriptors, Flex, localizationKeys, Spinner } from '../../customizables';
 import { Card, CardAlert, Divider, Header, useCardState, withCardStateProvider } from '../../elements';
 import { useInView } from '../../hooks';
+import { CreateOrganizationForm } from './CreateOrganization';
 import { PreviewListItems, PreviewListSpinner } from './shared';
 import { InvitationPreview } from './UserInvitationList';
 import { MembershipPreview, PersonalAccountPreview } from './UserMembershipList';
@@ -37,24 +40,11 @@ const useFetchList = () => {
 
 export const OrganizationListPage = withCardStateProvider(() => {
   const card = useCardState();
-  const clerk = useCoreClerk();
-  const { hidePersonal, createOrganizationMode, afterCreateOrganizationUrl, navigateCreateOrganization } =
-    useOrganizationListContext();
-
-  const environment = useEnvironment();
-
-  const { ref, userMemberships, userSuggestions, userInvitations } = useFetchList();
-
+  const { userMemberships, userSuggestions, userInvitations } = useFetchList();
   const isLoading = userMemberships?.isLoading || userInvitations?.isLoading || userSuggestions?.isLoading;
+  const hasAnyData = !!(userMemberships?.count || userInvitations?.count || userSuggestions?.count);
 
-  const hasNextPage = userMemberships?.hasNextPage || userInvitations?.hasNextPage || userSuggestions?.hasNextPage;
-
-  const handleCreateOrganizationClicked = () => {
-    if (createOrganizationMode === 'navigation') {
-      return navigateCreateOrganization();
-    }
-    return clerk.openCreateOrganization({ afterCreateOrganizationUrl });
-  };
+  const { hidePersonal } = useOrganizationListContext();
 
   return (
     <Card
@@ -80,91 +70,133 @@ export const OrganizationListPage = withCardStateProvider(() => {
         </Flex>
       )}
 
-      {!isLoading && (
-        <>
-          <Header.Root
-            sx={t => ({
-              padding: `${t.space.$none} ${t.space.$8}`,
-            })}
-          >
-            <Header.Title
-              localizationKey={localizationKeys(
-                !hidePersonal ? 'organizationList.title' : 'organizationList.titleWithoutPersonal',
-              )}
-            />
-            <Header.Subtitle
-              localizationKey={localizationKeys('organizationList.subtitle', {
-                applicationName: environment.displayConfig.applicationName,
-              })}
-            />
-          </Header.Root>
-          <Col
-            elementDescriptor={descriptors.main}
-            gap={4}
-          >
-            <PreviewListItems>
-              <PersonalAccountPreview />
-              {userMemberships?.data?.map(inv => {
-                return (
-                  <MembershipPreview
-                    key={inv.id}
-                    {...inv}
-                  />
-                );
-              })}
-
-              {!userMemberships?.hasNextPage &&
-                userInvitations?.data?.map(inv => {
-                  return (
-                    <InvitationPreview
-                      key={inv.id}
-                      {...inv}
-                    />
-                  );
-                })}
-
-              {!userMemberships?.hasNextPage &&
-                !userInvitations?.hasNextPage &&
-                userSuggestions?.data?.map(inv => {
-                  return (
-                    <SuggestionPreview
-                      key={inv.id}
-                      {...inv}
-                    />
-                  );
-                })}
-
-              {(hasNextPage || isLoading) && <PreviewListSpinner ref={ref} />}
-            </PreviewListItems>
-
-            <Divider
-              key={`divider`}
-              sx={t => ({
-                padding: `${t.space.$none} ${t.space.$8}`,
-              })}
-            />
-
-            <Flex
-              align='center'
-              justify='between'
-              sx={t => ({
-                padding: `${t.space.$none} ${t.space.$8}`,
-              })}
-            >
-              <Button
-                elementDescriptor={descriptors.button}
-                block
-                colorScheme='neutral'
-                size='sm'
-                variant='outline'
-                textVariant='buttonExtraSmallBold'
-                onClick={handleCreateOrganizationClicked}
-                localizationKey={localizationKeys('organizationList.action__createOrganization')}
-              />
-            </Flex>
-          </Col>
-        </>
-      )}
+      {!isLoading && <OrganizationListFlows showListInitially={!(hidePersonal && !hasAnyData)} />}
     </Card>
   );
 });
+
+const OrganizationListFlows = ({ showListInitially }: { showListInitially: boolean }) => {
+  const { navigateAfterSelectOrganization } = useOrganizationListContext();
+  const [isCreateOrganizationFlow, setCreateOrganizationFlow] = useState(!showListInitially);
+  return (
+    <>
+      {!isCreateOrganizationFlow && (
+        <OrganizationListPageList onCreateOrganizationClick={() => setCreateOrganizationFlow(true)} />
+      )}
+
+      {isCreateOrganizationFlow && (
+        <Box
+          sx={t => ({
+            padding: `${t.space.$none} ${t.space.$8}`,
+          })}
+        >
+          <CreateOrganizationForm
+            navigateAfterCreateOrganization={navigateAfterSelectOrganization}
+            onCancel={
+              showListInitially && isCreateOrganizationFlow ? () => setCreateOrganizationFlow(false) : undefined
+            }
+          />
+        </Box>
+      )}
+    </>
+  );
+};
+
+const OrganizationListPageList = (props: { onCreateOrganizationClick: () => void }) => {
+  const environment = useEnvironment();
+
+  const { ref, userMemberships, userSuggestions, userInvitations } = useFetchList();
+  const { hidePersonal } = useOrganizationListContext();
+
+  const isLoading = userMemberships?.isLoading || userInvitations?.isLoading || userSuggestions?.isLoading;
+  const hasNextPage = userMemberships?.hasNextPage || userInvitations?.hasNextPage || userSuggestions?.hasNextPage;
+
+  const handleCreateOrganizationClicked = () => {
+    props.onCreateOrganizationClick();
+  };
+  return (
+    <>
+      <Header.Root
+        sx={t => ({
+          padding: `${t.space.$none} ${t.space.$8}`,
+        })}
+      >
+        <Header.Title
+          localizationKey={localizationKeys(
+            !hidePersonal ? 'organizationList.title' : 'organizationList.titleWithoutPersonal',
+          )}
+        />
+        <Header.Subtitle
+          localizationKey={localizationKeys('organizationList.subtitle', {
+            applicationName: environment.displayConfig.applicationName,
+          })}
+        />
+      </Header.Root>
+      <Col
+        elementDescriptor={descriptors.main}
+        gap={4}
+      >
+        <PreviewListItems>
+          <PersonalAccountPreview />
+          {userMemberships?.data?.map(inv => {
+            return (
+              <MembershipPreview
+                key={inv.id}
+                {...inv}
+              />
+            );
+          })}
+
+          {!userMemberships?.hasNextPage &&
+            userInvitations?.data?.map(inv => {
+              return (
+                <InvitationPreview
+                  key={inv.id}
+                  {...inv}
+                />
+              );
+            })}
+
+          {!userMemberships?.hasNextPage &&
+            !userInvitations?.hasNextPage &&
+            userSuggestions?.data?.map(inv => {
+              return (
+                <SuggestionPreview
+                  key={inv.id}
+                  {...inv}
+                />
+              );
+            })}
+
+          {(hasNextPage || isLoading) && <PreviewListSpinner ref={ref} />}
+        </PreviewListItems>
+
+        <Divider
+          key={`divider`}
+          sx={t => ({
+            padding: `${t.space.$none} ${t.space.$8}`,
+          })}
+        />
+
+        <Flex
+          align='center'
+          justify='between'
+          sx={t => ({
+            padding: `${t.space.$none} ${t.space.$8}`,
+          })}
+        >
+          <Button
+            elementDescriptor={descriptors.button}
+            block
+            colorScheme='neutral'
+            size='sm'
+            variant='outline'
+            textVariant='buttonExtraSmallBold'
+            onClick={handleCreateOrganizationClicked}
+            localizationKey={localizationKeys('organizationList.action__createOrganization')}
+          />
+        </Flex>
+      </Col>
+    </>
+  );
+};
